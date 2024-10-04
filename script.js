2c3e8a173f327dcfcdecb625de9fd1ae8e9a2893
vladyslavbukator <103177221+Vladdddy@users.noreply.github.com>
2024-10-04 16:34:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const description = document.getElementById("description");
 
 menu.onclick = function () {
     navbar.style.display = "none";
+    description.style.display = "none";
     sideMenu.style.display = "flex";
     sideNavbar.style.display = "flex";
-    description.style.display = "none";
 };
 0
 sideMenu.onclick = function () {
     navbar.style.display = "flex";
+    description.style.display = "block";
     sideMenu.style.display = "none";
     sideNavbar.style.display = "none";
-    description.style.display = "block";
 };
